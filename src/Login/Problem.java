b99f28d3158d83fa325e348172bd2363cec41b13
Muhammed <muhammedaydin0580@gmail.com>
2023-07-27 17:25:41 +0300

    problem class from feature 10

---
+++ src/Login/Problem.java
@@ -0,0 +1,15 @@
+package Login;
+
+public class Problem {
+    public static void main(String[] args) {
+
+
+        for (int i = 0; i < ; i++) {
+            System.out.println(i);
+        }
+        // this line is important
+    }
+    
+    
+    
+}
